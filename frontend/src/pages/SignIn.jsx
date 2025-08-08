c8286fa556678861b8698dff3ec2d6b2abd357bf
Muskan200227 <sainimuskan731@gmail.com>
2025-08-08 20:50:09 +0530

    fix: adjust login/signup form height and center alignment

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import AuthForm from "../components/AuthForm";
 
 const SignIn = ({ isDarkMode }) => {  // Add isDarkMode prop
@@ -14,10 +14,11 @@ const SignIn = ({ isDarkMode }) => {  // Add isDarkMode prop
 
   return (
     <div>
-      <section className='flex justify-center items-center !w-full lg:flex !mt-5'>
+      <section className={`flex justify-center items-center !w-full lg:flex  ${
+        formType === "signup"? "!mt-20" :"!mt-15" }`}>
         <div className="w-full">
           {formType === 'signin' ? (
-            <section className='flex flex-col !space-y-7 w-full rounded-2xl justify-center items-center'>
+            <section className='flex flex-col w-full rounded-2xl justify-center items-center'>
               <AuthForm formType={formType} isDarkMode={isDarkMode} />
               <p className={isDarkMode ? 'text-white' : 'text-gray-800'}>
                 Don't have an account ? 
@@ -27,7 +28,7 @@ const SignIn = ({ isDarkMode }) => {  // Add isDarkMode prop
               </p>
             </section>
           ) : (
-            <section className='flex flex-col !space-y-7 rounded-2xl justify-center items-center'>
+            <section className='flex flex-col !space-y-8 rounded-2xl justify-center items-center'>
               <AuthForm formType={formType} isDarkMode={isDarkMode} />
               <p className={isDarkMode ? 'text-white' : 'text-gray-800'}>
                 Already have an account ?
